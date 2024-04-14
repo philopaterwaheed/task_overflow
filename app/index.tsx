2c5f434a4557794eea1de127339485a439096fce
philosan <philopaterwaheed9@gmail.com>
2024-04-14 13:50:55 +0200

    all_done

--- app/index.tsx
+++ app/index.tsx
@@ -2,21 +2,32 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Link, router } from "expo-router";
 import { useState ,useEffect} from 'react';
-import {FlatList, Button, StyleSheet,TextInput, Text, View } from "react-native";
-import { signout} from '../firebase/Auth';
+import {Pressable,FlatList, Button, StyleSheet,TextInput, Text, View } from "react-native";
+import {signout} from '../firebase/Auth';
 import {get_users  , find_user, update_user} from "../firebase/db";
 
 export default function Page() {
 const [user,setUser]=useState(null);
 const [tasks, setTasks]= useState([]); 
 const [inputText, setInputText] = useState('');
+const [donesel, setDoneSel] = useState();
+const [del, setDel] = useState();
 
 
-const Taskbox = ({title ,done, id , onPress , pressed , xpress}) =>{
+const Taskbox = ({title ,done, id , onPress , pressed , xpress, Style}) =>{
   return (
-    <View>
-	<Text>{(done)? "yes" : "no"}</Text>
-	<Text>{title}</Text>
+    <View style = {Style}>
+	<View style= {styles.checkBox}>
+	    <Pressable onPress = {pressed}>
+	    {
+		(!done)?
+		<Text>x</Text>:
+		<Text>o</Text>
+	    }
+	    </Pressable>
+	</View>
+	<Text style= {styles.text} >{title}</Text>
+	<Button title= "del" onPress = {onPress}/>
     </View>
   );
 }
@@ -42,7 +53,9 @@ const getTasksCloud = async () => {
       if (user != null){
 	const userObj = await find_user(JSON.parse(user).uid); 
 	console.log("from Tasks", userObj.data().Tasks);
-	setTasks(userObj.data().Tasks);
+	if (userObj.data()){
+	    setTasks(userObj.data().Tasks);
+	}
       }
   } catch (e) {
     setTasks(getTasksStore());
@@ -87,11 +100,30 @@ useEffect(() => {
     }
 }, [user]);
 useEffect(() => {
-    if (JSON.parse(user)){
+    if (JSON.parse(user) && tasks){
 	storeTasks(tasks);
 	update_user(JSON.parse(user).uid ,{Tasks:tasks});
     }
 }, [tasks]);
+useEffect(() => {
+
+const updatedData = tasks.map(item => {
+  if (item.id === donesel) {
+    return { ...item, done: !item.done }; // Update the age to 28 for object with id 2
+  }
+  return item; // For other items, return them as they are
+});
+if (updatedData){
+    setTasks(updatedData);
+}
+}, [donesel]);
+useEffect(() => {
+
+const updatedData = tasks.filter(item => item.id !== del);
+if (updatedData){
+    setTasks(updatedData);
+}
+}, [del]);
   return (
     <View style={styles.container}>
 	<View style={styles.inputArea}>
@@ -101,8 +133,12 @@ useEffect(() => {
 	    />
 	</View>
       <FlatList
+      style = {styles.list}
       data = {tasks} 
-      renderItem = {({item}) => <Taskbox  title = {item.title} done = {item.done}   />}
+      renderItem = {({item}) => <Taskbox Style ={styles.taskBox} title = {item.title} done = {item.done}
+      pressed = {()=>{setDoneSel(item.id);}}
+      onPress = {()=>{setDel(item.id);}}
+      />}
       keyExtractor = {(item)=>item.id
       }
       />
@@ -145,5 +181,30 @@ const styles = StyleSheet.create({
     marginHorizontal: "auto",
     height: 50,
     alignItems: "center",
+  },
+  list:{
+    flex: 1 , 
+
+  },
+  taskBox:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin : 5 , 
+    backgroundColor: "#63C7B2",
+    width: 250,
+    flexWrap : 'wrap',
+  },
+  text:{
+      width: '70%',
+  },
+  checkBox: {
+  width: 24,
+  height: 24 ,
+  backgroundColor : '#8E6C88' ,
+  borderRadius:  50,
+  alignItems: 'center' ,
+  borderColor :'#263D42' ,
+  borderWidth : 2 ,
   },
 });
--- firebase/db.ts
+++ firebase/db.ts
@@ -1,4 +1,5 @@
 
+import {  router } from "expo-router";
 import { db } from "./Config";
 import {
     getFirestore,
